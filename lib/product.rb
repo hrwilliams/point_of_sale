3b22e7f26e4986f81d667699d3cc9b8d1087d16a
Halle Williams and Stephanie Gurung <halle@>
2016-07-19 09:56:55 -0700

    validates that the product price is a number

--- lib/product.rb
+++ lib/product.rb
@@ -1,3 +1,4 @@
 class Product < ActiveRecord::Base
   validates(:name, {:presence => true, :length => { :maximum => 50}})
+  validates(:price, {:presence => true, numericality: true})
 end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,15 +1,16 @@
 describe(Product) do
   it("ensures the name is present") do
-    product = Product.new({:name => '', :price => 1.00})
+    product = Product.new({:name => ''})
     expect(product.save()).to(eq(false))
   end
   it("ensures the name is less than 50 characters") do
     product = Product.new({:name => "a".*(51)})
     expect(product.save()).to(eq(false))
   end
-  # it("ensures the price is a numerical entry") do
-  #
-  # end
+  it("ensures the price is a numerical entry") do
+    product = Product.new({:name => 'lamp', :price => 'lamp'})
+    expect(product.save()).to(eq(false))
+  end
   # it("sets the purchased value to false when initializing a product") do
   #
   # end
